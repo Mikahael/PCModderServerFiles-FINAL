b3189c82eb78f5ce1e514fbf15efd2ac78242d84
Mikahael <54455412+Mikahael@users.noreply.github.com>
2022-03-15 23:07:20 -0700

    Update cmdlogger.py

--- scripts/cmdlogger.py
+++ scripts/cmdlogger.py
@@ -1,7 +1,7 @@
 import bsInternal#made by blitz
 def log(msg,clientID):
     for i in bsInternal._getForegroundHostActivity().players:
-        if i.getInputDevice().getClientID()==clientID:
+        if i.getInputDevice().getClientID()==clientID:#not used. PCModder has made better one.
             client=i.get_account_id()
             name=i.getName()
 	f = open("log.txt","a")
