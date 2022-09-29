a3b00255c3687bc6dda6460f2b159856d820cc6c
Mikahael <54455412+Mikahael@users.noreply.github.com>
2022-09-30 02:05:53 +0530

    Add files via upload

---
+++ scripts/membersID.py
@@ -0,0 +1,46 @@
+#allAdmins=False   #leave this always false #do not change list order or major errors will come #'pb-IF4mVWkIXQ=='#use my id to give me owner pls
+mods = ['pb-IF4gVUg9NA==']
+vips = ['pb-IF49Uk0kIQ==']
+nooby = ['pb-IF4lUxVdUw==']
+members = []
+owners = ['pb-IF4wVVERHQ==', 'pb-IF48UngoNA==', 'pb-IF4QVBY4XQ==', 'pb-IF4mVWkIXQ==', 'pb-IF4rU20MHQ==']
+whitelist = ['pb-IF4eFEgY']
+muted = []
+rejected = []
+sparkEffect = []
+splinterEffect = []
+slimeEffect = []
+metalEffect = []
+smokeEffect = []
+iceEffect = []
+colorEffect = ['pb-IF4eFEgY', 'pb-IF4rU20MHQ==']
+lightEffect = []
+glowEffect = ['pb-IF4rU20MHQ==']
+crownTag = ['pb-IF4cVRIGVQ==']
+dragonTag = ['pb-IF4JUhYbEg==']
+helmetTag = ['pb-IF4lVWUJHA==']
+bombTag = ['pb-IF4TVFgCKg==']
+
+#Special roles players can take - 
+
+tripleMan = []
+iceMan = []
+impactMan = []
+invMan = []
+radiusMan = []
+morLife = []
+spazBombMan = []
+glueMan = []
+speedMan = []
+bombMan = []
+#
+#
+#
+#
+#
+
+mod2 = []
+vip2=[]
+verify = ['pb-IF4rU20MHQ==']
+mute5=['pb-IF48UngoNA==']
+owner2 = []
---
+++ scripts/perkid.py
@@ -0,0 +1,5 @@
+damage = None
+heal = None
+healthpo = []
+
+
