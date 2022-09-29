9951e73273f205e4e5f6730941bc4d56c5b71fd5
Mikahael <54455412+Mikahael@users.noreply.github.com>
2022-09-30 01:54:54 +0530

    Delete scripts directory

--- scripts/cmdlogger.py
+++
@@ -1,9 +0,0 @@
-import bsInternal#made by blitz
-def log(msg,clientID):
-    for i in bsInternal._getForegroundHostActivity().players:
-        if i.getInputDevice().getClientID()==clientID:#not used. PCModder has made better one.
-            client=i.get_account_id()
-            name=i.getName()
-	f = open("log.txt","a")
-	f.write(name+"----->"+client+"------>"+ msg+"\n")
-	f.close()
