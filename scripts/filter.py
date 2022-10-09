f64d26cdffd4b0df180c051bc34829bd6ee5fa7a
Mikahael <54455412+Mikahael@users.noreply.github.com>
2022-10-09 10:38:38 +0530

    Update filter.py

--- scripts/filter.py
+++ scripts/filter.py
@@ -2,7 +2,9 @@ import bs
 import bsInternal
 import settings
 
-f_words = ["fuck","shit","tits"]
+f_words= ['cum','cumshot','boob','boobies','tit','titz','fuck','fucker','shit','shithead','pussy','PuSSy','fucked','bitch','bitches','bietch','sex','Sex','bastard','Fuck','Fucker','Cum','Bitch']
+
+name_filter = ['cum','cumshot','boob','boobies','tit','titz','fuck','fucker','shit','shithead','pussy','PuSSy','fucked','bitch','bitches','bietch','sex','Sex','bastard','Fuck','Fucker','Cum','Bitch']
 
 warndict = {}
 
@@ -17,13 +19,13 @@ def check(cid):
 	if settings.enableChatFilter:
 		if warndict[cid] == 1:
 			bsInternal._disconnectClient(int(cid))
-			bs.screenMessage("Kicking For Misbehave", color = (1,0,0))
+			bs.screenMessage("Kicking For Misbehave", color = (1,1,1), transient=True, clients=[clientID])
 			warndict.pop(cid)
 		elif warndict[cid] == 0:
 			warndict[cid] = 1
 
 def warn(clientID):
 	if settings.enableChatFilter:
-		bs.screenMessage("Warning!!! Do Not Misbehave", color = (1,0,0), transient=True, clients=[clientID])
+		bs.screenMessage("Warning!!! Do Not Misbehave", color = (1,1,1), transient=True, clients=[clientID])
     	if warndict[clientID] == 0:
-    		bs.screenMessage("Last Chance Warning 1/2", color = (1,0,0), transient=True, clients=[clientID])
+    		bs.screenMessage("Last Chance Warning 1/2", color = (1,1,1), transient=True, clients=[clientID])
