7c5eb68f4adf359d14f0cc47cb462eb5a9fab873
Mikahael <54455412+Mikahael@users.noreply.github.com>
2023-08-26 23:37:26 +0530

    Update getPermissionsHashes.py

--- data/scripts/getPermissionsHashes.py
+++ data/scripts/getPermissionsHashes.py
@@ -5,7 +5,7 @@ topperslist = []
 effectCustomers = {}
 customlist = {'pb-IF4eFEgY': '\xee\x81\x89BelowAverage\xee\x81\x89'}
 ownerHashes = ['pb-IF4eFEgY','pb-IF4rU20MHQ==','pb-IF4BUlQBJw==']
-coownerHashes = ['pb-IF4wU00DEA==' ,'pb-IF4GUkcPJw==','pb-JiNJARFfXEFCVVdAGUdWV1xDEkZcRFBG' ,'pb-IF5UVVAmFw==','pb-IF4wVXoOBg==']
+coownerHashes = []
 surroundingObjectEffect = []
 sparkEffect = []
 smokeEffect = []
@@ -17,7 +17,7 @@ slimeEffect = []
 metalEffect = []
 dragonHashes = []
 customtagHashes=[]
-whitelist = {'pb-IF4rU20MHQ==': '\xee\x80\xb0PC591985'}
+whitelist = {}
 
 #donot change the order of the list
 #to enable/disable commands and effects for top 5 players goto settings.py
