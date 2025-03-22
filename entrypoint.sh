9bc35f6141b0d20108dfdd02f3a5fe761cc0d070
Mikahael <54455412+Mikahael@users.noreply.github.com>
2025-03-22 08:11:31 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-mv /home/me/* /home/container
-cd /home/container
+cd /home/container/bombsquad_server
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 ${MODIFIED_STARTUP}
-echo "Ensuring BombSquad files are in /home/container..."
 
-# Move files to the correct location if necessary
-if [ ! -f "/home/container/bombsquad_server" ]; then
-    echo "Files not found in /home/container. Moving..."
-    mv /BombSquad_Server/* /home/container/ || true
-fi
+# Ensure the server file is executable
+chmod +x bombsquad_server
 
-# Set proper permissions
-chmod +x /home/container/bombsquad_server
-
-# Verify file existence
-ls -lah /home/container
-
-# Start the server
-exec /home/container/bombsquad_server "$@"
+# Start the BombSquad server
+./bombsquad_server
