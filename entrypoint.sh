e38294f6f0088e344edec1abab3bf76b0192967f
Mikahael <54455412+Mikahael@users.noreply.github.com>
2025-03-22 08:06:48 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,19 @@ cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 ${MODIFIED_STARTUP}
-echo hewllow_worldd
-pwd
-ls -al
-./bombsquad_server
+echo "Ensuring BombSquad files are in /home/container..."
+
+# Move files to the correct location if necessary
+if [ ! -f "/home/container/bombsquad_server" ]; then
+    echo "Files not found in /home/container. Moving..."
+    mv /BombSquad_Server/* /home/container/ || true
+fi
+
+# Set proper permissions
+chmod +x /home/container/bombsquad_server
+
+# Verify file existence
+ls -lah /home/container
+
+# Start the server
+exec /home/container/bombsquad_server "$@"
