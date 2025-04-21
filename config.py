9ce0d9e1893897a364cf8a03eed9587fdab93dc2
Mikahael <54455412+Mikahael@users.noreply.github.com>
2025-04-22 02:04:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# place any of your own overrides here.
+# place any of your own overrides here. 
 # see bombsquad_server for details on what you can override
 # examples (uncomment to use):
 # config['partyName'] = 'My Awesome Party'
